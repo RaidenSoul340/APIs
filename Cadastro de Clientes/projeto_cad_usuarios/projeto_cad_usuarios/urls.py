28573232e63d00056020a809fb204230f3a6686d
João Rodrigo <73110330+RaidenSoul340@users.noreply.github.com>
2023-10-30 22:19:32 -0300

    Delete Cadastro de Clientes/projeto_cad_usuarios directory

--- Cadastro de Clientes/projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app_cad_usuarios import views
-
-urlpatterns = [
-    #rota, view responsavel, nome referencia
-    #cadastro.com
-    path("", views.home, name="home"),
-]
